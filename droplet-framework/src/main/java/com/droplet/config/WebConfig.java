ff367ebacb66cd08a0e30916aa163d650210417b
霜暮雪 <Frost.rA9@gmail.com>
2022-10-24 18:59:55 +0800

    [FastJson2] 添加FastJson2扩展包 | 升级FastJson2版本 | 配置FastJsonHttpMessageConverter

--- droplet-framework/src/main/java/com/droplet/config/WebConfig.java
+++ droplet-framework/src/main/java/com/droplet/config/WebConfig.java
@@ -1,12 +1,27 @@
 package com.droplet.config;
 
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.support.spring.http.converter.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * 跨域配置
+     *
+     * @param registry 配置
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -15,5 +30,18 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastJson2HttpMessageConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJson2Config = new FastJsonConfig();
+        fastJson2Config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJson2Config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+        fastJson2Config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat);
+        fastJson2HttpMessageConverter.setFastJsonConfig(fastJson2Config);
+        fastJson2HttpMessageConverter.setDefaultCharset(StandardCharsets.UTF_8);
+        fastJson2HttpMessageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        converters.add(0, fastJson2HttpMessageConverter);
     }
 }
