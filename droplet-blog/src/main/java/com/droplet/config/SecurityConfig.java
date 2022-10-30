eb7e25470df9a868190a8acb02c7e4d76bb7443e
霜暮雪 <Frost.rA9@gmail.com>
2022-10-30 20:24:37 +0800

    [Authentication] 添加登录校验过滤器

--- droplet-blog/src/main/java/com/droplet/config/SecurityConfig.java
+++ droplet-blog/src/main/java/com/droplet/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.droplet.config;
 
+import com.droplet.filter.JwtAuthenticationTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -9,11 +10,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    public SecurityConfig(JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter) {
+        this.jwtAuthenticationTokenFilter = jwtAuthenticationTokenFilter;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -39,7 +47,8 @@ public class SecurityConfig {
                 .anyRequest().permitAll();
 
         http.logout().disable();
-
+        // 将JwtAuthenticationTokenFilter添加到SpringSecurity过滤器链
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         // 允许跨域
         http.cors();
         return http.build();
