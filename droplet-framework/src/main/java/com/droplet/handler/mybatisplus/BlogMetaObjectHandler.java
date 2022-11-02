ca7363548e539dfff8ec94576c17fd8ec23e4557
霜暮雪 <Frost.rA9@gmail.com>
2022-11-02 19:37:25 +0800

    [MyBatis-Plus] 配置MyBatis-Plus字段自动填充

---
+++ droplet-framework/src/main/java/com/droplet/handler/mybatisplus/BlogMetaObjectHandler.java
@@ -0,0 +1,42 @@
+package com.droplet.handler.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.droplet.utils.SecurityUtils;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class BlogMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * 插入元对象字段填充（用于插入时对公共字段的填充）
+     *
+     * @param metaObject 元对象
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        Long userId = null;
+        try {
+            userId = SecurityUtils.getUserId();
+        } catch (Exception e) {
+            e.printStackTrace();
+            userId = -1L;
+        }
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("createBy", userId, metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+        this.setFieldValByName("updateBy", userId, metaObject);
+    }
+
+    /**
+     * 更新元对象字段填充（用于更新时对公共字段的填充）
+     *
+     * @param metaObject 元对象
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+        this.setFieldValByName(" ", SecurityUtils.getUserId(), metaObject);
+    }
+}
