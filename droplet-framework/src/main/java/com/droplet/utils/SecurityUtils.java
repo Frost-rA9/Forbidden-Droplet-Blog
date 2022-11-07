1d97783bd4d9765eeca243569e15568d338d7f66
霜暮雪 <Frost.rA9@gmail.com>
2022-11-07 08:24:49 +0800

    [Utils] 修正SpringSecurity工具类错误

--- droplet-framework/src/main/java/com/droplet/utils/SecurityUtils.java
+++ droplet-framework/src/main/java/com/droplet/utils/SecurityUtils.java
@@ -6,6 +6,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
     /**
+     * 获取用户
+     *
+     * @return 用户
+     */
+    public static LoginUser getLoginUser() {
+        return (LoginUser) getAuthentication().getPrincipal();
+    }
+
+    /**
      * 获取Authentication
      *
      * @return Authentication
@@ -15,29 +24,21 @@ public class SecurityUtils {
     }
 
     /**
-     * 获取登录用户
-     *
-     * @return 登录用户
-     */
-    public static LoginUser getLoginUser() {
-        return (LoginUser) getAuthentication().getAuthorities();
-    }
-
-    /**
-     * 获取登录用户Id
-     *
-     * @return 登录用户Id
-     */
-    public static Long getUserId() {
-        return getLoginUser().getUser().getId();
-    }
-
-    /**
-     * 判断是否为管理员
+     * 判断用户是否为管理员
      *
      * @return 结果
      */
     public static Boolean isAdmin() {
-        return getUserId() != null && 1L == getUserId();
+        Long id = getLoginUser().getUser().getId();
+        return id != null && 1L == id;
+    }
+
+    /**
+     * 获取用户Id
+     *
+     * @return 用户Id
+     */
+    public static Long getUserId() {
+        return getLoginUser().getUser().getId();
     }
 }
