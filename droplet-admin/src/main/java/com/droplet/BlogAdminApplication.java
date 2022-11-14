a66769f4b5dcd6e323fc847a8f12a6bcf7b27480
霜暮雪 <Frost.rA9@gmail.com>
2022-11-14 13:58:23 +0800

    [Admin] 初始化

--- droplet-admin/src/main/java/com/droplet/BlogAdminApplication.java
+++ droplet-admin/src/main/java/com/droplet/BlogAdminApplication.java
@@ -1,2 +1,11 @@
-package com.droplet;public class BlogAdminApplication {
+package com.droplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class);
+    }
 }
